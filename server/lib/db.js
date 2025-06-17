901505eff849071453062e142a396a00c1d54191
SHAMIL-VAKKAYIL <muhammedshamil470@gmail.com>
2025-06-17 12:50:16 +0530

    final build10

--- server/lib/db.js
+++ server/lib/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const DbConnect = mongoose.connect(process.env.MONGO_URI).then(() => {
+export const DbConnect = mongoose.connect('mongodb://admin:pass123@216.10.242.120:27017/EBoutique?authSource=admin').then(() => {
     console.log('Connected to MongoDB')
 }).catch(err => console.error(err))
