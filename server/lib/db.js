80ae95d6203112c3f608ff0f2a303f5a4bc99d80
muhammed shamil <108108797+SHAMIL-VAKKAYIL@users.noreply.github.com>
2025-06-18 06:49:54 +0530

    Update db.js

--- server/lib/db.js
+++ server/lib/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const DbConnect = mongoose.connect('mongodb://admin:pass123@216.10.242.120:27017/EBoutique?authSource=admin').then(() => {
+export const DbConnect = mongoose.connect('mongodb://admin:pass123@mongodb:27017/EBoutique?authSource=admin').then(() => {
     console.log('Connected to MongoDB')
 }).catch(err => console.error(err))
